920aafe3ab31559b9655709c6d6bb6c85ec601c2
change <wen_change@qq.com>
2016-09-10 17:00:52 +0800

    main cpp

--- Thread-Pool/main.cpp
+++ Thread-Pool/main.cpp
@@ -1,18 +1,7 @@
 ﻿#include "NetService.h"
 
-#ifdef _DEBUG
-#define DEBUG_CLIENTBLOCK new( _CLIENT_BLOCK, __FILE__, __LINE__)
-#else
-#define DEBUG_CLIENTBLOCK
-#endif  // _DEBUG
-#define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
-#include <crtdbg.h>
-#ifdef _DEBUG
-#define new DEBUG_CLIENTBLOCK
-#endif  // _DEBUG
-
 #include <random>
+#include <iostream>
 
 class testCMD : public CmdHandleDelegate
 {
